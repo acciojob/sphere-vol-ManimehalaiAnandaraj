9fd8c4bd8dc493d869b20bcea366706d5a7e031f
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-23 16:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	const radius=document.getElementById("radius").value;
+	if(!isNaN(radius) && radius >=0)
+	{
+		const volume= (4/3) * Math.PI*Math.pow(radius,3);
+		return parseFloat(volume.toFixed(4));
+	}
+	else
+	{
+		alert("NaN");
+		document.getElementById("volume").value="";
+	}
+	
   
 } 
 
